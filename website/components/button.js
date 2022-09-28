0cf832651748ae00d076158586a81182744f10da
Tom Benbow <tom@hellomoon.net>
2022-09-28 18:53:49 -0500

    Update minor button style

--- website/components/button.js
+++ website/components/button.js
@@ -1,15 +1,15 @@
 export default function Button({target="_self", href="#", children, className, variation="major"}) {
   const buttonVariations = {
-    major: "text-white bg-gradient-to-tr from-red-custom to-yellow-custom py-2",
-    minor: " text-white bg-orange-500",
-    outline: "border-[3px] border-cyan-800 text-cyan-800 py-1",
+    major: "text-white bg-gradient-to-tr from-red-custom to-yellow-custom py-2 text-h5",
+    minor: " text-white bg-orange-500 text-h5",
+    outline: "border-[3px] border-white text-white py-1 text-body-md",
   }
   return (
     <a
       href={href}
       target={target}
       type="button"
-      className={`inline-flex text-h5 leading-10 rounded-lg px-6 lg:px-12 outline-none focus:outline-none ${className} ${buttonVariations[variation]}`}
+      className={`inline-flex leading-10 rounded-lg px-6 lg:px-12 outline-none focus:outline-none ${className} ${buttonVariations[variation]}`}
     >
       {children}
     </a>
