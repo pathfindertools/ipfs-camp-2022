e0b20070e6a0d8009f4b285ef36228052a075c2f
Tom Benbow <tom@hellomoon.net>
2022-09-28 12:02:23 -0500

    Mobile styling touchups

--- website/components/accordian.js
+++ website/components/accordian.js
@@ -17,14 +17,14 @@ const AccordianItem = ({question, answer}) => {
   return (
     <div className="border-b border-cyan-400 first:border-t-0 last:border-b-0">
       <div className="relative p-4 pb-3 cursor-pointer" onClick={() => setActive(!active)}>
-        <h4 className="text-body-lg">{question}</h4>
+        <h4 className="text-body1 lg:text-body-lg">{question}</h4>
         <div className={`absolute right-5 top-5 ${active ? 'opacity-10 transform rotate-180' : ''}`}>
           <svg width="16" height="19" viewBox="0 0 16 19" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M8.92265 0.551269L7.5125 0.551269C7.04245 0.551269 6.78934 0.804374 6.78934 1.27442L6.78934 13.3873L2.48657 9.66303C2.16115 9.37377 1.76341 9.37377 1.47415 9.73535L0.64252 10.7839C0.353258 11.1455 0.389415 11.4709 0.750993 11.7963L7.5125 17.7624C7.7656 17.9793 7.91023 18.0878 8.1995 18.0878C8.56107 18.0878 8.63339 17.9793 8.8865 17.7624L15.6842 11.7963C16.0457 11.4709 16.0819 11.1455 15.7926 10.7839L14.961 9.73535C14.6717 9.33761 14.274 9.33761 13.9124 9.62687L9.64581 13.3511L9.64581 1.27442C9.64581 0.804374 9.3927 0.551269 8.92265 0.551269Z" fill="white"/>
           </svg>
         </div>
       </div>
-      <div className={`text-body-lg p-4 pt-0 border-cyan-400 ${active ? '' : 'hidden'}`}>
+      <div className={`text-body1 lg:text-body-lg p-4 pt-0 border-cyan-400 ${active ? '' : 'hidden'}`}>
         <Markdown>{answer}</Markdown>
       </div>
     </div>
--- website/components/hero.js
+++ website/components/hero.js
@@ -4,7 +4,7 @@ import Button from './button'
 
 const Lockup = (props) => {
   return (
-    <img src={props.logo} className='mr-auto my-20  h-16 lg:h-24'/>
+    <img src={props.logo} className='mr-auto my-10 lg:my-20 h-16 lg:h-24'/>
   )
 }
 
@@ -19,31 +19,31 @@ export default function Hero({ config }) {
 
 
 
-        <div className="relative w-full pt-10 pb-28 lg:pt-20">
+        <div className="relative w-full pt-10 pb-8 lg:pb-28 lg:pt-20">
           <div className="relative z-1 mx-auto min-h-full lg:ml-0 flex flex-col gap-y-3">
             <Lockup name={config.devent.name} logo={config.devent.logo} tagline={config.devent.tagline} />
-            <div className='basis-1/3 mb-5'>
+            <div className='lg:basis-1/3 mb-5'>
               {/* <div className="text-2xl">
                 {dateRangeStr(config.devent.dateStart, config.devent.dateEnd)}{config.devent.location && ` • ${config.devent.location}`}
               </div> */}
-              <div className="text-h2 mb-6 w-3/4">
+              <div className="text-h2 mb-6 lg:w-3/4">
                 {config.devent.tagline}
               </div>
-              <div className="text-body-lg w-3/4">
+              <div className="text-body-lg lg:w-3/4">
                 <Markdown >{config.devent.description}</Markdown>
               </div>
-              <div className="flex gap-4">
+              <div className="flex flex-col lg:flex-row gap-4 mt-8">
                 {config.devent.ticketLink &&
-                  <Button href={config.devent.ticketLink} className="mt-8" target="_blank">Get Tickets</Button>
+                  <Button href={config.devent.ticketLink} target="_blank">Get Tickets</Button>
                 }
                 {config.devent.speakLink &&
-                  <Button href={config.devent.speakLink} className="mt-8" target="_blank" variation="outline">Apply to Speak</Button>
+                  <Button href={config.devent.speakLink} target="_blank" variation="outline">Apply to Speak</Button>
                 }
                 {config.devent.rsvpLink &&
-                  <Button href={config.devent.rsvpLink} className="mt-8" target="_blank">Get Updates</Button>
+                  <Button href={config.devent.rsvpLink} target="_blank">Get Updates</Button>
                 }
                 {config.devent.recapLink &&
-                  <Button href={config.devent.recapLink} className="mt-8" target="_blank">Recap</Button>
+                  <Button href={config.devent.recapLink} target="_blank">Recap</Button>
                 }
               </div>
             </div>
